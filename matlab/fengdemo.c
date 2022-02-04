7fc8f10ccaefb88c23a3f82268800f411994a4de
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-04 12:36:40 -0500

    add matlab engine c example

---
+++ matlab/fengdemo.c
@@ -0,0 +1,79 @@
+/*
+ *	engdemo.c
+ *
+ *	A simple program to illustrate how to call MATLAB
+ *	Engine functions from a C program.
+ *
+ * Copyright 1984-2016 The MathWorks, Inc.
+ * All rights reserved
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "engine.h"
+
+#define  BUFSIZE 256
+
+int main(int argc, char* argv[]){
+	Engine *ep;
+	mxArray *T = NULL, *result = NULL;
+	char buffer[BUFSIZE+1];
+	double time[10] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
+
+	/*
+	 * Call engOpen with a NULL string. This starts a MATLAB process
+     * on the current host using the command "matlab".
+	 */
+	if (!(ep = engOpen(""))) {
+		fprintf(stderr, "\nCan't start MATLAB engine\n");
+		return EXIT_FAILURE;
+	}
+
+	/*
+	 * PART I
+	 *
+	 * For the first half of this demonstration, send data
+	 * to MATLAB, analyze the data, and plot the result.
+	 */
+
+	/*
+	 * Create a variable for the data
+	 */
+	T = mxCreateDoubleMatrix(1, 10, mxREAL);
+	memcpy((void *)mxGetPr(T), (void *)time, sizeof(time));
+	/*
+	 * Place the variable T into the MATLAB workspace
+	 */
+	engPutVariable(ep, "T", T);
+
+	/*
+	 * Evaluate a function of time, distance = (1/2)g.*t.^2
+	 * (g is the acceleration due to gravity)
+	 */
+	engEvalString(ep, "D = .5.*(-9.8).*T.^2;");
+
+	/*
+	 * Plot the result
+	 */
+  if(argc > 0 && strcmp(argv[1],"-v")==0){
+	engEvalString(ep, "plot(T,D);");
+	engEvalString(ep, "title('Position vs. Time for a falling object');");
+	engEvalString(ep, "xlabel('Time (seconds)');");
+	engEvalString(ep, "ylabel('Position (meters)');");
+
+	/*
+	 * use fgetc() to pause long enough to be
+	 * able to see the plot
+	 */
+	printf("Hit return to continue\n\n");
+	fgetc(stdin);
+  }
+	/*
+	 * We're done for Part I! Free memory, close MATLAB figure.
+	 */
+	printf("Done for Part I.\n");
+	mxDestroyArray(T);
+	engEvalString(ep, "close;");
+
+	return EXIT_SUCCESS;
+}
