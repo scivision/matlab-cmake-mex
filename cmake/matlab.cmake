c30a84124255ef4382ab36e441d150a1f546cb61
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-30 02:51:09 -0400

    need threads

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -3,6 +3,8 @@ include(CheckSourceCompiles)
 
 set(CMAKE_CXX_STANDARD 11)
 
+find_package(Threads)
+
 matlab_get_mex_suffix(${Matlab_ROOT_DIR} Matlab_MEX_SUFFIX)
 
 
@@ -26,7 +28,7 @@ endfunction(matlab_libpath)
 
 # --- check C engine
 
-set(CMAKE_REQUIRED_LIBRARIES ${Matlab_ENG_LIBRARY} ${Matlab_MX_LIBRARY})
+set(CMAKE_REQUIRED_LIBRARIES ${Matlab_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
 set(CMAKE_REQUIRED_INCLUDES ${Matlab_INCLUDE_DIRS})
 
 check_source_compiles(C
@@ -49,14 +51,14 @@ Matlab_engine_C
 
 if(NOT DEFINED Matlab_engine_CXX)
   message(CHECK_START "Performing Test Matlab_engine_CXX")
-  try_compile(Matlab_engine_CXX
+  try_compile(_engine_CXX
   ${CMAKE_CURRENT_BINARY_DIR}/engine_cxx
   ${CMAKE_CURRENT_SOURCE_DIR}/src/eng_demo.cpp
   CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${Matlab_INCLUDE_DIRS}"
-  LINK_LIBRARIES ${Matlab_LIBRARIES}
+  LINK_LIBRARIES ${Matlab_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT}
   OUTPUT_VARIABLE err
   )
-  if(Matlab_engine_CXX)
+  if(_engine_CXX)
     set(Matlab_engine_CXX true CACHE BOOL "Matlab CXX engine")
     message(CHECK_PASS "Success")
   else()
