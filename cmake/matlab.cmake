c66ef3696f95297672555e10fbaf9eb2167497eb
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-29 21:27:20 -0400

    cmake: functionalize

---
+++ cmake/matlab.cmake
@@ -0,0 +1,30 @@
+matlab_get_mex_suffix(${Matlab_ROOT_DIR} Matlab_MEX_SUFFIX)
+
+if(APPLE)
+  set(MATLAB_ARCH maci64)
+elseif(WIN32)
+  set(MATLAB_ARCH win64)
+elseif(UNIX)
+  set(MATLAB_ARCH glnxa64)
+else()
+  message(FATAL_ERROR "Unknown platform")
+endif()
+
+
+function(matlab_libpath test_names)
+
+if(APPLE)
+  set_tests_properties(${test_names} PROPERTIES
+  ENVIRONMENT_MODIFICATION DYLD_LIBRARY_PATH=path_list_prepend:${Matlab_BINARIES_DIR}:${Matlab_ROOT_DIR}/sys/os/${MATLAB_ARCH}
+  )
+elseif(WIN32)
+  set_tests_properties(${test_names} PROPERTIES
+  ENVIRONMENT_MODIFICATION PATH=path_list_prepend:${Matlab_BINARIES_DIR}
+  )
+else()
+  set_tests_properties(${test_names} PROPERTIES
+  ENVIRONMENT_MODIFICATION LD_LIBRARY_PATH=path_list_prepend:${Matlab_BINARIES_DIR}:${Matlab_ROOT_DIR}/sys/os/${MATLAB_ARCH}
+  )
+endif()
+
+endfunction(matlab_libpath)
