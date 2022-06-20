e5b1d41dd2acd30a7522e48d7fd3d284617304a7
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-20 10:42:02 -0400

    put mex/ and engine/ in own dir for clarity

--- mex/TestBlas.m
+++ mex/TestBlas.m
@@ -1,16 +1,12 @@
-classdef TestDemo < matlab.unittest.TestCase
+classdef TestBlas < matlab.unittest.TestCase
 
 methods(Test)
-
-function test_true(tc)
-tc.assertTrue(true)
-end
 
 function test_blas(tc)
 a = eye(3);
 b = magic(3);
 x = matrixMultiply(a, b);
-disp(x)
+tc.verifyEqual(x, a*b)
 end
 
 end
