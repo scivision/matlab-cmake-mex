c9c432b367c0cfba1c64b17d1618b22ae6b53c17
scivision <scivision@users.noreply.github.com>
2023-01-12 09:57:37 -0500

    msvc: quiet nuisance warnings

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -3,6 +3,8 @@ include(CheckSourceCompiles)
 
 set(CMAKE_CXX_STANDARD 11)
 
+add_compile_definitions($<$<AND:$<BOOL:${MSVC}>,$<COMPILE_LANGUAGE:C,CXX>>:_CRT_SECURE_NO_WARNINGS>)
+
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU" AND
   CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
   add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-fallow-invalid-boz>)
